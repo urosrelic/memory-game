e18699354923457f27eb539e599a9b659b318036
marijajevtic <urosrelicc@gmail.com>
2024-01-31 15:00:53 +0100

    feat: add pokemon logo

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,10 @@
-export const Title = () => {
+export const Title = ({ score, bestScore }) => {
   return (
     <header>
-      <h1>Memory Game</h1>
+      <div className='logo'>
+        <img id='pokemon-logo' src='/pokemon-23.svg'></img>
+        <h3>Memory game</h3>
+      </div>
     </header>
   );
 };
