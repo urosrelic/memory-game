c41726febddcbc646e1e777a4ccca1d604399ba3
marijajevtic <urosrelicc@gmail.com>
2024-01-31 15:30:30 +0100

    chore: add score to title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,6 +5,10 @@ export const Title = ({ score, bestScore }) => {
         <img id='pokemon-logo' src='/pokemon-23.svg'></img>
         <h3>Memory game</h3>
       </div>
+      <div className='score-container'>
+        <div className='score'>Score: {score}</div>
+        <div className='best-score'>Best score: {bestScore}</div>
+      </div>
     </header>
   );
 };
